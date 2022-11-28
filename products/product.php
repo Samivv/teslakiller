a04637ebd1f09fd20f5929d2905495c4e77801e5
Milla Tuppurainen <you@example.com>
2022-11-28 17:45:09 +0200

    yhden tuotteen hakeminen id:n perusteella

--- products/product.php
+++ products/product.php
@@ -1,28 +1,15 @@
 <?php
-require '../inc/functions.php';
-require '../inc/headers.php';
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
 
-// Haetaan kaikki tuotteet tuoteryhmän perusteella
-$url = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'), PHP_URL_PATH);
+$url = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
 $param = explode('/', $url);
-$category_id = $param[1];
+$product_id = $param[1];
 
 try {
-    $db = createSqliteConnection('../allset.db');
-    $sql = "select * from category where category_id = $category_id";
-    $query = $db->query($sql);
-    $category = $query->fetch(PDO::FETCH_ASSOC);
-   
-    $sql = "select * from product where category_id = $category_id";
-    $query = $db->query($sql);
-    $products = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        header('HTTP/1.1 200 OK');
-        echo json_encode(array(
-            "category" => $category['category_name'],
-            "products" => $products
-        ));
+  $db = createSqliteConnection('../allset.db');
+  selectAsJson($db, "select * from product where id = $product_id");
 }
 catch (PDOException $pdoex) {
-    returnError($pdoex);
+  returnError($pdoex);
 }
