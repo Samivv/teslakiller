106e200b70204d1bfbacaad7ccef314ebd4228bd
Samivv <101732172+Samivv@users.noreply.github.com>
2022-12-05 18:24:18 +0200

    +getcategories

---
+++ products/getcategories.php
@@ -0,0 +1,18 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+try {
+    $db = createSqliteConnection("../allset.db");
+    $sql = "SELECT * FROM category";
+    $query = $db->query ($sql);
+    $data = $query->fetchAll(PDO::FETCH_ASSOC);
+    header('HTTP/1.1 200 OK');
+    print json_encode($data);
+    
+} catch (PDOException $pdoex) {
+        returnError($pdoex);
+}
+
+
+?>
--- src/admin.js
+++ src/admin.js
@@ -1,9 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import {Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
+
 import axios from 'axios';
 
 function AdminPanel() {
+  const [data, setData] = useState([]);
+  const getCategories = () => {
+    axios.get('http://localhost:3000/products/getcategories.php')
+    .then(res=> {
+      setData(res.data);
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+ 
+  
   const addCategory = (e) => {
     e.preventDefault();
     const data = {
@@ -23,13 +38,26 @@ function AdminPanel() {
         <div className="container">
           <div className="row">
             <div className="col-md-6">
+              <h1>Categories</h1>
+              <Button onClick={getCategories}>Get Categories</Button>
+              <ul>
+                {data.map((item, index) => {
+                  return <li key={index}>{item.category_id} - {item.category_name}</li>
+                })}
+              </ul>
+              </div>
+              </div>
+            </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
               <Form onSubmit={addCategory}>
                 <Form.Group controlId="formBasicEmail">
                   <Form.Label>Category Name</Form.Label>
                   <Form.Control type="text" placeholder="Enter Category Name" name="name" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
-                  Submit
+                  Add a category
                 </Button>
               </Form>
               </div>
