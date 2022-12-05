4eec770ffbe7aa79d004bb44587202aea28741db
Samivv <101732172+Samivv@users.noreply.github.com>
2022-12-05 15:58:09 +0200

    linkitys "adminpaneeliin"

--- products/addcategory.php
+++ products/addcategory.php
@@ -3,15 +3,15 @@ require_once '../inc/functions.php';
 require_once '../inc/headers.php';
 
 $input = json_decode(file_get_contents('php://input'));
-$name = $_GET['name'];
-$name = filter_var($name, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$name = filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
 try {
     $db = createSqliteConnection("../allset.db");
     $sql = "insert into category (category_name) values ('$name')";
     $query = $db->query ($sql);
     $data = array('id' => $db->lastInsertId(), 'name' => $name);
     header('HTTP/1.1 200 OK');
-    echo json_encode($data);
+    print json_encode($data);
 } catch (PDOException $pdoex) {
         returnError($pdoex);
 }
